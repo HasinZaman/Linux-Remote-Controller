b38218bb69d42bda474c7389b8234dc2b6f67720
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-08-04 13:01:04 -0400

    Create VolumeController.js

---
+++ pages/VolumeController/res/javascript/VolumeController.js
@@ -0,0 +1,116 @@
+volumeUpCond = false
+volumeDownCond = false
+muteCond = false
+volume = 0
+
+portraitMode = true
+
+//volume up and down buttons are hold
+$("button#volumeUp").on("vmousedown",function(){
+	if (!volumeDownCond)
+	{
+		volumeUpCond = true
+
+		$(this).attr("data-pressCond",1)
+	}
+})
+$("button#volumeDown").on("vmousedown",function(){
+	if (!volumeUpCond)
+	{
+		volumeDownCond = true
+
+		$(this).attr("data-pressCond",1)
+	}
+})
+
+//volume up and down buttons are released
+$("button#volumeUp").on("vmouseup",function(){
+	volumeUpCond = false
+
+	$(this).attr("data-pressCond",0)
+})
+$("button#volumeDown").on("vmouseup",function(){
+	volumeDownCond = false
+
+	$(this).attr("data-pressCond",0)
+})
+
+//volume mute
+$("button#volumeMute").click(function(){
+	buttonPress("toggleMute")
+})
+$("button#playPause").click(function(){
+	buttonPress("togglePlayPause")
+})
+
+function buttonPress(buttonName)
+{
+	$.ajax
+	({
+		type:"POST",
+		data:
+		{
+			page:"VolumeController",
+			action:"buttonPress",
+			button:buttonName
+		}
+	})
+}
+
+function getState()
+{
+	$.ajax
+	({
+		type:"POST",
+		data:
+		{
+			page:"VolumeController",
+			action:"getState"
+		},
+		success: getStateResponse
+	})
+}
+
+function getStateResponse(result)
+{
+	console.log(result)
+	setTimeout(update, 100)
+}
+
+function update()
+{
+	if($("button#volumeUp").attr("data-pressCond") == 1){
+		buttonPress("increaseVolume")
+		$("button#volumeMute").attr("data-pressCond",0)
+	}else if($("button#volumeDown").attr("data-pressCond") == 1){
+		buttonPress("decreaseVolume")
+		$("button#volumeMute").attr("data-pressCond",0)
+	}
+
+	portraitMode = window.screen.width <= window.screen.height
+
+	if(portraitMode)
+	{
+		$("#volumeBar").css("width", volume+"%")
+	}
+	else
+	{
+		$("#volumeBar").css("height", volume+"%")
+	}
+
+	$("button[data-pressCond=1]").addClass("pressed")
+	$("button[data-pressCond=0]").removeClass("pressed")
+
+	if($("button#volumeMute").attr("data-pressCond") == 1)
+	{
+		$("#volumeBar").addClass("muted")
+	}
+	else
+	{
+		$("#volumeBar").removeClass("muted")
+	}
+
+	getState()
+}
+
+update()
