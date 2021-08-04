b17d9b74a657b1192dbf610d18fe524491136cc0
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-08-04 14:41:46 -0400

    Added volume up, down and mute functionality to VolumeController.py

--- pages/VolumeController.py
+++ pages/VolumeController.py
@@ -13,6 +13,7 @@ class VolumeController(Page):
 		self.name = "VolumeController"
 		self.muted = False
 		self.volume = self.mixer.getvolume()[0]
+		self.volumeStep = 10
 
 	def action(self, data, response):
 		'''
@@ -23,16 +24,33 @@ class VolumeController(Page):
 			response (dictionary): dictionary of response that will be filled with output
 		'''
 		if self.validActionSource(data["page"]):
-			if data["action"] == "getState":
-				self.mixer = alsaaudio.Mixer()
-				self.volume = self.mixer.getvolume()[0]
+			self.mixer = alsaaudio.Mixer()
+			self.volume = int(self.mixer.getvolume()[0])
 
+			if data["action"] == "getState":
 				response["response"] = True
-				response["volume"] = int(self.volume)
+				response["volume"] = self.volume
 				response["muted"] = self.muted
 				return None;
 			elif data["action"] == "buttonPress":
 				response["response"] = True
+				
+				if data["button"] == "increaseVolume":
+					m.setvolume(self.volume + self.volumeStep)
+				elif data["button"] == "decreaseVolume":
+					m.setvolume(self.volume - self.volumeStep)
+				elif data["button"] == "toggleMute":
+					self.muted = not self.muted
+					if self.muted:
+						m.setvolume(0)
+					else:
+						m.setvolume(self.volume)
+				elif data["button"] == "togglePlayPause":
+					pass
+				elif data["button"] == "skipNext":
+					pass
+				elif data["button"] == "skipPrev":
+					pass
 				return None;
 		response["response"] = False
 		return None;
