b58c81d936b43b8705008776e8f45342783e4301
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-08-04 00:12:53 -0400

    Updated do_GET method in Server.py

--- Server.py
+++ Server.py
@@ -30,6 +30,8 @@ class Server(BaseHTTPRequestHandler):
 
     def do_GET(self):
         if self.path == '/':
+        requestedFile = None
+        if self.path == '/' or self.path == '/index.html':
             self.path = '/index.html'
         file_to_open = None
         try:
@@ -58,14 +60,51 @@ class Server(BaseHTTPRequestHandler):
             else:
                 file_to_open = "File not found"
                 self.send_response(404)
+            requestedFile = open(self.path[1:],mode = "r").read().split("CONTENT")
 
+            content = "".join([self.__makeButton(page.page.name) for page in pages])
             
         except:
             file_to_open = "File not found"
             self.send_response(404)
         self.end_headers()
         self.wfile.write(file_to_open)
+            requestedFile = "{0}{1}{2}".format(requestedFile[0], content, requestedFile[1])
 
+            requestedFile = bytes(requestedFile, 'utf-8')
+            self.send_response(200)
+        elif os.path.exists(self.path[1:]) and self.path.split(".")[-1] != "html":
+            requestedFile = open(self.path[1:],mode = "rb").read()
+                    
+            self.send_response(200)
+
+            if self.path[-3:] == "svg":
+                self.send_header('Content-type', 'image/svg+xml')
+                        
+            elif self.path[-3:] == "gif":
+                self.send_header('Content-type', 'image/gif')
+                
+            elif self.path[-4:] == "jpeg" or self.path[-3:] == "jpg":
+                self.send_header('Content-type', 'image/jpeg')
+                        
+            elif self.path[-3:] == "png":
+                self.send_header('Content-type', 'image/png')
+                        
+            elif self.path[-4:] == "html":
+                 self.send_header('Content-type', 'text/html')
+                        
+            elif self.path[-3:] == "css" or self.path[-7:-4] == "css" :
+                self.send_header('Content-type', 'text/css')
+                
+        elif os.path.exists("pages//{0}//{0}.html".format(self.path[1:])):
+            print("pages//{0}//{0}.html".format(self.path[1:]))
+            requestedFile = open("pages//{0}//{0}.html".format(self.path[1:]), mode = "rb").read()
+            self.send_response(200)
+        else:
+             requestedFile = "File not found"
+             self.send_response(404)    
+        self.end_headers()
+        self.wfile.write(requestedFile)
 
     def dataPrepare(self,dataRaw):
         content_length = int(dataRaw.headers['Content-Length'])
