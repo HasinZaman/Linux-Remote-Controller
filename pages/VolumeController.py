026174708ff96c24a4ceddfe21378310f37492a3
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-08-04 13:03:23 -0400

    Create VolumeController.py

---
+++ pages/VolumeController.py
@@ -0,0 +1,34 @@
+from Page import *
+
+class VolumeController(Page):
+	'''
+	VolumeController class updates the volume of the linux system hosting the webserver
+	'''
+	def __init__(self):
+		'''
+		constructor initializes VolumeController
+		'''
+		self.name = "VolumeController"
+		self.muted = False
+
+	def action(self, data, response):
+		'''
+		action method responds to POST call
+
+		Paramaters:
+			data (dictionary): Dictonary of json data sent by POST
+			response (dictionary): dictionary of response that will be filled with output
+		'''
+		if self.validActionSource(data["page"]):
+			if data["action"] == "getState":
+				response["response"] = True
+				#response["response"] = True
+				#response["response"] = True
+				return None;
+			elif data["action"] == "buttonPress":
+				response["response"] = True
+				return None;
+		response["response"] = False
+		return None;
+
+page = VolumeController()
