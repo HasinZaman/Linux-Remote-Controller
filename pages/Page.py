367b36981662f6af95836e1e9094d2bd15f4e71d
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-08-04 00:10:35 -0400

    Created Pages.py abstract class

---
+++ pages/Page.py
@@ -0,0 +1,8 @@
+class Page():
+	def __init__(self):
+		self.name = "Page"
+		pass
+	def validActionSource(self, pageName):
+		return pageName == name
+	def action(self, data, server):
+		raise Exception("Unimplemented method")
