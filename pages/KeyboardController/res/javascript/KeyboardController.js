1eb7862b9ded39e99becde48a6264a643198178a
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-08-05 11:42:10 -0400

    Added client end backspace functionality

--- pages/KeyboardController/res/javascript/KeyboardController.js
+++ pages/KeyboardController/res/javascript/KeyboardController.js
@@ -1,7 +1,40 @@
+var backspace = false
+$("button#backSpace").on("vmousedown",function(){
+	if (!backspace)
+	{
+		backspace = true
+	}
+})
+$("button#backSlash").on("vmouseup",function(){
+	backspace = false
+})
+
+$("button#enter").click(function(){
+	buttonPress("enter")
+})
+
+function buttonPress(buttonName)
+{
+	$.ajax
+	({
+		type:"POST",
+		data:
+		{
+			page:"KeyboardController",
+			action: "buttonPress",
+			button: buttonName
+		}
+	})
+}
 
 function update()
 {
 	var keySequence = ""
+
+	if(backspace)
+	{
+		buttonPress("backspace")
+	}
 
 	if($("#keyboardInput").val() !== "" && $("#keyboardInput").val() !== undefined)
 	{
