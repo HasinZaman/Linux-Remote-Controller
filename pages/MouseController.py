835b1a56277676ed331b96f8f6e0fb71e8e0a9e8
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-08-05 12:59:25 -0400

    Create MouseController.py

---
+++ pages/MouseController.py
@@ -0,0 +1,23 @@
+from Page import *
+
+class MouseController(Page):
+	'''
+	MouseController class updates the volume of the linux system hosting the webserver
+	'''
+	def __init__(self):
+		'''
+		constructor initializes MouseController
+		'''
+		self.name = "MouseController"
+		
+	def action(self, data, response):
+		'''
+		action method responds to POST call
+		Paramaters:
+			data (dictionary): Dictonary of json data sent by POST
+			response (dictionary): dictionary of response that will be filled with output
+		'''
+		if self.validActionSource(data["page"]):
+			pass
+
+page = MouseController()	
