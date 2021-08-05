8414c1f98d61a439428ec5e368a55e09228d7309
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-08-05 12:19:09 -0400

    Keyboard controller (#3)
    * Create KeyboardController.py
    
    * Create KeyboardController.html
    
    * Create KeyboardController.js
    
    * Create style.css
    
    * Create style.scss
    
    * Update KeyboardController.py
    
    * Update KeyboardController.py
    
    * Change directory keyController to keyboardController
    
    * Update KeyboardController.html
    
    * Updated style
    
    * Update KeyboardController.html
    
    * Changed folder style to css
    
    * Update KeyboardController.html
    
    * Update style.scss
    
    * Update KeyboardController.html
    
    * updated CSS
    
    * Updated CSS
    
    * Updated CSS
    
    * Update CSS
    
    * Created update method in KeyboardController.js
    
    * Implemented typing functionality in KeyboardController.py
    
    * added ajax post call to keyboardController.js
    
    * added update call on load in KeyboardController.js
    
    * Update KeyboardController.py
    
    * Update KeyboardController.py
    
    * Update KeyboardController.py
    
    * Update KeyboardController.py
    
    * Update KeyboardController.js
    
    * Update KeyboardController.py
    
    * Update KeyboardController.js
    
    * Update KeyboardController.py
    
    * Update KeyboardController.html
    
    * Added backslash and enter button
    
    * Update KeyboardController.html
    
    * Removed debug print
    
    * Added button text
    
    * Updated Css
    
    * Update KeyboardController.html
    
    * Updated Css
    
    * Added backspace and enter server side functionality
    
    * update backslash -> backspace
    
    * Added client end backspace functionality
    
    * Update KeyboardController.html
    
    * Update KeyboardController.py
    
    * Update KeyboardController.js
    
    * Update KeyboardController.js
    
    * Update KeyboardController.py
    
    * Updated css
    
    * Update css
    
    * Update css

---
+++ pages/KeyboardController.py
@@ -0,0 +1,49 @@
+from Page import *
+import subprocess
+import time
+
+class KeyboardController(Page):
+	'''
+	KeyboardController class updates the volume of the linux system hosting the webserver
+	'''
+	def __init__(self):
+		'''
+		constructor initializes KeyboardController
+		'''
+		self.name = "KeyboardController"
+		
+	def action(self, data, response):
+		'''
+		action method responds to POST call
+		Paramaters:
+			data (dictionary): Dictonary of json data sent by POST
+			response (dictionary): dictionary of response that will be filled with output
+		'''
+		if self.validActionSource(data["page"]):
+			if data["action"] == "keySequence":
+				for letter in data["keySequence"]:
+					if letter in "abcdefghijklmnopqrstuvwxzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890":
+						subprocess.call(["xdotool", "type", letter])
+						time.sleep(0.1)
+						response["response"] = True
+						return
+					elif letter == "+":
+						subprocess.call(["xdotool", "type", " "])
+						time.sleep(0.1)
+						response["response"] = True
+						return
+			elif data["action"] == "buttonPress":
+				if data["button"] == "enter":
+					subprocess.call(["xdotool", "key", "Return"])
+					time.sleep(0.1)
+					response["response"] = True
+					return
+				elif data["button"] == "backspace":
+					subprocess.call(["xdotool", "key", "BackSpace"])
+					time.sleep(0.1)
+					response["response"] = True
+					return
+		response["response"] = False
+		return None
+
+page = KeyboardController()
---
+++ pages/KeyboardController/res/javascript/KeyboardController.js
@@ -0,0 +1,59 @@
+var backspace = false
+$("button#backSpace").on("vmousedown",function(){
+	if (!backspace)
+	{
+		backspace = true
+	}
+})
+
+$("button#backSpace").on("vmouseup",function(){
+	backspace = false
+})
+
+$("button#enter").click(function(){
+	buttonPress("enter")
+})
+
+function buttonPress(buttonName)
+{
+	$.ajax
+	({
+		type:"POST",
+		data:
+		{
+			page:"KeyboardController",
+			action: "buttonPress",
+			button: buttonName
+		}
+	})
+}
+
+function update()
+{
+	var keySequence = ""
+
+	if(backspace)
+	{
+		buttonPress("backspace")
+	}
+
+	if($("#keyboardInput").val() !== "" && $("#keyboardInput").val() !== undefined)
+	{
+		$.ajax
+		({
+			type:"POST",
+			data:
+			{
+				page: "KeyboardController",
+				action: "keySequence",
+				keySequence: $("#keyboardInput").val()
+			}
+		})
+
+		$("#keyboardInput").val("")
+	}
+
+	setTimeout(update, 100)
+}
+
+update()
