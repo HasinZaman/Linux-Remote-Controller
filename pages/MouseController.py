6d9a8e671fb812218b38887149f56da5f6cdd1f4
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-08-06 17:38:54 -0400

    Mouse controller (#4)
    * Create MouseController.py
    
    * Create MouseController.html
    
    * created css
    
    * Create MouseController.js
    
    * Update MouseController.js
    
    * Added left and right mouse click button
    
    * Updated MouseController.css
    
    * Update menu css in universal css file
    
    * Update MouseController.js
    
    * Update MouseController.html
    
    * Update MouseController.html
    
    * Updated css
    
    * Update MouseController.js
    
    * Update MouseController.js
    
    * added mouse sensitivity slider
    
    * Added mousePos delta
    
    * added mouse click buttons
    
    * Added server side debug messages
    
    * updated sensitivity html
    
    * Update MouseController.js
    
    * Added responses
    
    * Update MouseController.js
    
    * Update MouseController.js
    
    * Update MouseController.js
    
    * Update MouseController.js
    
    * Update MouseController.py
    
    * Update MouseController.js
    
    * Update MouseController.py
    
    * Update MouseController.py
    
    * Update MouseController.js
    
    * Update MouseController.py
    
    * Update CSS
    
    * Update style.css
    
    * Update MouseController.html
    
    * Update CSS
    
    * Update MouseController.html
    
    * Update MouseController.js
    
    * Update MouseController.js
    
    * Update MouseController.js
    
    * Update MouseController.js
    
    * Update MouseController.js

---
+++ pages/MouseController.py
@@ -0,0 +1,52 @@
+from Page import *
+import subprocess
+
+class MouseController(Page):
+	'''
+	MouseController class updates the volume of the linux system hosting the webserver
+	'''
+	def __init__(self):
+		'''
+		constructor initializes MouseController
+		'''
+		self.name = "MouseController"
+		self.mouseDown = [False, False]
+		
+	def action(self, data, response):
+		'''
+		action method responds to POST call
+		Paramaters:
+			data (dictionary): Dictonary of json data sent by POST
+			response (dictionary): dictionary of response that will be filled with output
+		'''
+		if self.validActionSource(data["page"]):
+			if data["action"] == "buttonPress":
+				if not self.mouseDown[0] and not self.mouseDown[1]:
+					if data["button"] == "leftDown":
+						subprocess.call(["xdotool", "mousedown", "1"])
+						self.mouseDown[0] = True
+					elif data["button"] == "rightUp":
+						subprocess.call(["xdotool", "mousedown", "3"])
+						self.mouseDown[1] = True
+
+				elif self.mouseDown[0] and not self.mouseDown[1]:
+					if data["button"] == "leftUp":
+						subprocess.call(["xdotool", "mouseup", "1"])
+						self.mouseDown[0] = False
+
+				elif not self.mouseDown[1] and self.mouseDown[1]:
+					if data["button"] == "rightDown":
+						subprocess.call(["xdotool", "mouseup", "3"])
+						self.mouseDown[1] = False
+				else:
+					print("invalid state")
+				response["response"] = True
+				return
+			elif data["action"] == "move":
+				delta = [data["deltaX"], data["deltaY"]]
+
+
+				subprocess.call(["xdotool", "mousemove_relative", "--", delta[0], delta[1]])
+				response["response"] = True
+
+page = MouseController()	
