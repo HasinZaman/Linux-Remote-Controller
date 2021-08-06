a471b832d28bccd2b23f288783d4f2b3fd69fef7
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-08-06 10:17:24 -0400

    Added mousePos delta

--- pages/MouseController/res/javascript/MouseController.js
+++ pages/MouseController/res/javascript/MouseController.js
@@ -3,6 +3,9 @@ var mouseDown = false
 var mousePos = [undefined, undefined]
 var mouseTap = []
 
+var delta = [undefined, undefined]
+
+//touch pad controls
 $("#touchpad").on("vmousedown",function(event){
 	mousePos[0] = [event.pageX, event.pageY]
 })
@@ -16,6 +19,14 @@ function update()
 	if(mousePos[0] != undefined && mousePos[1] != undefined)
 	{
 		console.log(mousePos)
+
+		for(var i1 = 0; i1 < 2; i1++)
+		{
+			delta[i1] = mousePos[1][i1] - mousePos[0][i1]
+		}
+
+		console.log(delta)
+
 		mousePos = [undefined, undefined]
 	}
 
